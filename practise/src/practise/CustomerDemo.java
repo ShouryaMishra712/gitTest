44e4446d7ae09f365529dcf8fc2824a7604d7cac
shourya <shourya.mujalde@gmail.com>
2020-07-25 20:45:01 +0530

    ccc

---
+++ practise/src/practise/CustomerDemo.java
@@ -0,0 +1,12 @@
+package practise;
+
+public class CustomerDemo {
+
+	public static void main(String[] args) {
+		Customer[] cust = new Customer[4];
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
